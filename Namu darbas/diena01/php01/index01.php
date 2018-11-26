5c781b166587a1328fd0b76d711415e2d6d78ee5
monicaelektronica <36565001+monicaelektronica@users.noreply.github.com>
2018-11-26 21:31:37 +0200

    pridetas namu darbas php01

---
+++ Namu darbas/diena01/php01/index01.php
@@ -0,0 +1,20 @@
+<?php
+$miestas = 'Vilnius';
+$salis = 'Lietuva';
+$populiacija = 574221; ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <li><?php echo $miestas ?></li>
+      <li><?php echo $salis ?></li>
+      <li> <?php echo $populiacija ?></li>
+    </ul>
+
+  </body>
+</html>
