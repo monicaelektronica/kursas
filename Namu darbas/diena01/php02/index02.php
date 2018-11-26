b468757626148856de8b03bad544f8d0d8c37a23
monicaelektronica <36565001+monicaelektronica@users.noreply.github.com>
2018-11-26 21:32:22 +0200

    pridetas namu darbas php02

---
+++ Namu darbas/diena01/php02/index02.php
@@ -0,0 +1,16 @@
+<?php
+$miestas = 'Vilnius';
+$salis = 'Lietuva';
+$populiacija = 574221; ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><?php echo  $miestas .'- Lietuvos sostinė'; ?></p>
+    <p><?php echo "$miestas ir $salis sparčiai auga"; ?></p>
+  </body>
+</html>
