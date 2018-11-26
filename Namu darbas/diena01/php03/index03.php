cab85bee19999078671829d8adda1a94f6267032
monicaelektronica <36565001+monicaelektronica@users.noreply.github.com>
2018-11-26 21:33:03 +0200

    pridetas namu darbas php03

---
+++ Namu darbas/diena01/php03/index03.php
@@ -0,0 +1,29 @@
+<?php
+$x = 10;
+$y = 7;
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+          $z = $x + $y;
+          echo "$x + $y = $z" ."<br>";
+     $z = $x - $y;
+          echo "$x - $y = $z" . "<br>";
+     $z = $x * $y;
+          echo "$x * $y = $z" . "<br>";
+     $z = $x / $y;
+          echo "$x / $y = $z" . "<br>";
+     $z = $x % $y;
+          echo "$x % $y = $z" . "<br>";?>
+
+
+
+
+  </body>
+</html>
