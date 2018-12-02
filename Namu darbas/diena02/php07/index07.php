a7d933ce2f1d0da57d24e83b3efae32be2176db5
monicaelektronica <36565001+monicaelektronica@users.noreply.github.com>
2018-12-02 17:26:37 +0200

    pridetas namu darbas php07

---
+++ Namu darbas/diena02/php07/index07.php
@@ -0,0 +1,30 @@
+<?php
+$cities4 = ['Tokijas', 'Vasingtonas', 'Maskva', 'Londonas'];
+  print_r($cities4);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <?php
+      for ($i=0; $i < count($cities4); $i++) {
+        echo "<li>$cities4[$i]</li>";
+      } ?>
+
+    </ul>
+    <ul>
+      <?php
+      foreach ($cities4 as $cities) {
+        echo $cities .'<br>';
+      } ?>
+    </ul>
+
+
+  </body>
+</html>
