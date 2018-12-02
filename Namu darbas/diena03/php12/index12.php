a88eaa32251f36245e5898172ace7fca193cb14c
monicaelektronica <36565001+monicaelektronica@users.noreply.github.com>
2018-12-02 17:33:43 +0200

    pridetas namu darbas php12

---
+++ Namu darbas/diena03/php12/index12.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST["submit"])){
+  echo ("Vardas:" . $_POST["vardas"] . "<br> \n");
+  echo ("Pavarde:" . $_POST["pavarde"]);
+  exit();
+}
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <form action= "<?php $_PHP_SELF; ?>" method="POST">
+       Vardas: <input type="text" name="vardas"/>
+       Pavarde: <input type="text" name="pavarde"/>
+       <input type="submit" name="submit" value="submit">
+
+     </form>
+
+   </body>
+ </html>
