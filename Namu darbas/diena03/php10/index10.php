7728b345f9be1f5dc2e88b3143e9cfaf7a515027
monicaelektronica <36565001+monicaelektronica@users.noreply.github.com>
2018-12-02 17:32:58 +0200

    pridetas namu darbas php10

---
+++ Namu darbas/diena03/php10/index10.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Funkcijos su parametrais</title>
+  </head>
+  <body>
+    <?php
+    function daugyba($num1, $num2) {
+      $daugyba = $num1 * $num2;
+      echo "Staciakampio plotas yra: $daugyba";
+    }
+
+    daugyba(5, 8);
+     ?>
+
+  </body>
+</html>
